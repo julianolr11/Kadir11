048f4c581f57dcf5e1e238afb0fccd4a7c43f0ea
Juliano Ramos <julianolr11@gmail.com>
2025-11-29 14:07:38 -0300

    test: import constants.mjs to remove ESM typeless warning

--- test/loadSpeciesData.test.js
+++ test/loadSpeciesData.test.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 describe('loadSpeciesData', function () {
   it('returns known species when directories exist', async function () {
-    const constants = await import('../scripts/constants.js');
+    const constants = await import('../scripts/constants.mjs');
     await constants.loadSpeciesData(path.resolve(__dirname, '..'));
     assert.ok(constants.specieData.Besta, 'Expected Besta species to be loaded');
   });
